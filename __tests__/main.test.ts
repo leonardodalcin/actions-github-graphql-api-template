f132753c4df899084a125cb4ba580b6126b6ce43
leonardo dalcin <leon.dalcin@gmail.com>
2020-04-13 11:05:23 -0300

    refactor: makes structural changes

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,5 +1,5 @@
-import {wait} from '../src/wait'
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+// import {wait} from '../src/wait'
+// test('throws invalid number', async () => {
+//   const input = parseInt('foo', 10)
+//   await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+// })
